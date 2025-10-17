17f34ad7d02d14aa50c902fac4196f547c4dc7d4
BumjinKim <21900104@handong.ac.kr>
2025-10-17 20:09:53 +0900

    feat(main): Application 클래스 시작점 구현 - Setting 메소드 및 run 메소드 삽입

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // Setting
+        CalculatorController calculatorController = CalculatorController.settingClass();
+        // Run
+        calculatorController.runCalculator();
     }
 }
