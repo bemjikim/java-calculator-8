c31ceaae6c1a517aea6d4598b56f029caeddce73
BumjinKim <21900104@handong.ac.kr>
2025-10-17 16:49:18 +0900

    feat(view): InputView 클래스 구조 생성 - printInputUI 메서드를 통해 사용자가 입력 받을 때 보이는 문구 생성

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+import calculator.enums.UITemplate;
+
+public class InputView {
+    public void printInputUI() {
+        System.out.println(UITemplate.INPUT_MESSAGE.getMessage());
+    }
+}
