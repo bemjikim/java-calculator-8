0bfc6880cc3d8b8f56ab1747ca62caa6113a77e7
BumjinKim <21900104@handong.ac.kr>
2025-10-20 19:09:53 +0900

    feat(controller): CalculatorController 클래스 수정 - 결과 값 출력

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,29 +1,36 @@
 package calculator.controller;
 
+import calculator.service.CalculatorService;
 import calculator.service.DelimiterService;
 import calculator.service.InputService;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class CalculatorController {
 
     private final InputView inputView;
+    private final OutputView outputView;
     private final InputService inputService;
     private final DelimiterService delimiterService;
+    private final CalculatorService calculatorService;
 
-    private CalculatorController(InputView inputView, InputService inputService, DelimiterService delimiterService) {
+    private CalculatorController(InputView inputView, OutputView outputView, InputService inputService, DelimiterService delimiterService, CalculatorService calculatorService) {
         this.inputView = inputView;
+        this.outputView = outputView;
         this.inputService = inputService;
         this.delimiterService = delimiterService;
+        this.calculatorService = calculatorService;
     }
 
     // 생성 메소드
     public static CalculatorController settingClass() {
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
         InputService inputService = new InputService();
         DelimiterService delimiterService = new DelimiterService();
+        CalculatorService calculatorService = new CalculatorService();
 
-
-        return new CalculatorController(inputView, inputService, delimiterService);
+        return new CalculatorController(inputView, outputView, inputService, delimiterService, calculatorService);
     }
 
     // 동작
@@ -35,6 +42,8 @@ public class CalculatorController {
         // 2. 구분자 나누기
         long[] split_num = delimiterService.splitDelimiter(user_input);
 
+        // 3. 더하기
+        long result = calculatorService.cal_nums(split_num);
+        outputView.printOutputUI(result);
     }
-
 }
