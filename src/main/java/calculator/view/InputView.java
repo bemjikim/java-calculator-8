534f61bad68bf12c6fe97a1d62a2c81cd383a5e8
BumjinKim <21900104@handong.ac.kr>
2025-10-20 19:00:26 +0900

    feat(view): InputView 클래스 comment 수정 - comment 수정

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,6 +3,7 @@ package calculator.view;
 import calculator.enums.UITemplate;
 
 public class InputView {
+    // 사용자 입력 UI 메서드
     public void printInputUI() {
         System.out.println(UITemplate.INPUT_MESSAGE.getMessage());
     }
