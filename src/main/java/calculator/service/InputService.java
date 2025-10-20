c2b7d05db197eba42cefe375e9328b7ce99edc47
BumjinKim <21900104@handong.ac.kr>
2025-10-20 18:59:08 +0900

    feat(service): InputService클래스 comment 수정 - comment 수정

--- src/main/java/calculator/service/InputService.java
+++ src/main/java/calculator/service/InputService.java
@@ -2,14 +2,13 @@ package calculator.service;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputService {
-
-    // 1. user input을 받음
+    // User Input을 처리하는 메서드
     public String getInput() {
-        return checkEmpty(Console.readLine());
+        return checkUserInput(Console.readLine());
     }
-    // TODO 2. input 값 검증하기
 
-    private String checkEmpty(String user_input) {
+    // User Input이 공백인지 확인하는 메서드
+    private String checkUserInput(String user_input) {
         return user_input.isEmpty() ? "0" : user_input;
     }
 }
