292f24a63da8e78b6d28644e3e4b8bc50b9ee182
BumjinKim <21900104@handong.ac.kr>
2025-10-20 19:01:05 +0900

    feat(service): DelimiterService 클래스 기능 추가 - 입력값 형식 체크하는 기능 추가

--- src/main/java/calculator/service/DelimiterService.java
+++ src/main/java/calculator/service/DelimiterService.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static calculator.util.Validator.validateCustomDelimiter;
-import static calculator.util.Validator.validateNumber;
+import static calculator.util.Validator.*;
 
 public class DelimiterService {
     // Delimiter에 CustomDelimiter 추가 및 분리 메서드
@@ -14,10 +13,13 @@ public class DelimiterService {
 
         while (user_input.startsWith("//")) {
             int customDelimiterIndex = validateCustomDelimiter(user_input);
-            delimiter += checkSepcialDelimiter(user_input, customDelimiterIndex) + user_input.substring(2, customDelimiterIndex);
+            String extract_Delimiter = user_input.substring(2, customDelimiterIndex);
+
+            delimiter += checkSepcialDelimiter(extract_Delimiter) + extract_Delimiter;
             user_input = user_input.substring(customDelimiterIndex + 2);
         }
 
+        validateFormat(user_input, delimiter);
         String[] numbers = user_input.split(delimiter);
 
         return Arrays.stream(numbers)
@@ -26,7 +28,7 @@ public class DelimiterService {
     }
 
     // Custom Delimiter 특수문자 처리 메서드
-    private String checkSepcialDelimiter(String user_input, int customDelimiterIndex) {
+    private String checkSepcialDelimiter(String user_input) {
         String specialRegex = "[\\\\.^$|?*+()\\[\\]{}]";
 
         Pattern pattern = Pattern.compile(specialRegex);
@@ -39,13 +41,10 @@ public class DelimiterService {
         return "|";
     }
 
-
     // 숫자 유효성 검사 메서드
     private long checkNumberArray(String numbers) {
         validateNumber(numbers);
-        return Integer.parseInt(numbers.trim());
+        return Long.parseLong(numbers.trim());
     }
-
-
 }
 
