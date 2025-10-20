67c9f7c0863a1ddcccf0d998f29e1e9f81d8fdc2
BumjinKim <21900104@handong.ac.kr>
2025-10-20 18:55:06 +0900

    feat(enums): ErrorMessageTemplate 메시지 추가 - 합 계산 때 오버플로우 발생시 예외 처리 메시지 - 덧셈할 문자열의 포멧 예외 처리 메시지

--- src/main/java/calculator/enums/ErrorMessageTemplate.java
+++ src/main/java/calculator/enums/ErrorMessageTemplate.java
@@ -2,13 +2,11 @@ package calculator.enums;
 
 public enum ErrorMessageTemplate {
     INVALID_NONPOSITIVE_NUMBER("입력 값에 양수가 아닌 수는 포함될 수 없습니다: "),
+    INVALID_SUM_OVERFLOW("결과 값에 오버플로우가 발생했습니다: "),
     INVALID_NUMBER_FORMAT("입력 값에 유효하지 않는 숫자 or 구분자는 포함될 수 없습니다: "),
-    // TODO: 더 많은 에러 메시지들 생성
-    // "입력 값에 유효하지 않는 형식은 포함될 수 없습니다."
     INVALID_CUSTOM_DELIMITER_FORMAT("커스텀 구분자의 형식이 올바르지 않습니다."),
-    INVALID_LINEAR_DELIMITER("문자열에서 연속된 구분자는 허용하지 않습니다."),
+    INVALID_CALCULATE_FORMAT("덧셈할 문자열의 포멧을 지켜주세요 ex) 1,2,3"),
     INVALID_ONE_MORE_CUSTOM_DELIMITER("커스텀 구분자는 한 개의 문자로 이루어져야 합니다.");
-
 
     private final String message;
 
--- src/main/java/calculator/model/Calculator.java
+++
@@ -1,6 +0,0 @@
-package calculator.model;
-
-// 최종 결과 계산
-public class Calculator {
-    // TODO: 계산 하는 로직 추가
-}
