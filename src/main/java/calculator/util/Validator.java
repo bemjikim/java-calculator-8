6acc82b6ef15a20c6c85d4ce049eb7091385ff14
BumjinKim <21900104@handong.ac.kr>
2025-10-20 19:02:20 +0900

    feat(util): Validator 클래스 기능 추가 및 comment 추가 - 입력값의 형식을 검증하는 메서드 추가

--- src/main/java/calculator/util/Validator.java
+++ src/main/java/calculator/util/Validator.java
@@ -2,16 +2,14 @@ package calculator.util;
 
 import calculator.enums.ErrorMessageTemplate;
 
-import java.util.Objects;
+import java.util.Arrays;
 
 public class Validator {
     // 숫자 유효성 검증 메서드
     public static void validateNumber(String numbers) {
-        if (Objects.equals(numbers, "")) {
-            throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_LINEAR_DELIMITER.getMessage());
-        }
         try {
-            int linear_num = Integer.parseInt(numbers.trim());
+            long linear_num = Long.parseLong(numbers.trim());
+
             if (linear_num <= 0) {
                 throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_NONPOSITIVE_NUMBER.getMessage() + linear_num);
             }
@@ -20,11 +18,26 @@ public class Validator {
         }
     }
 
+    // 문자열 형식 검증 메서드
+    public static void validateFormat(String user_input, String delimiter) {
+        String[] numbers = Arrays.stream(user_input.split(delimiter))
+                                 .filter(s -> !s.isEmpty())
+                                 .toArray(String[]::new);
+        int num_delimiter_length = user_input.length();
+        int linear_num_length = String.join("", numbers).length();
+
+        int split_num_length = numbers.length;
+        int delimiter_length = num_delimiter_length - linear_num_length;
+
+        if (split_num_length - delimiter_length != 1) {
+            throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_CALCULATE_FORMAT.getMessage());
+        }
+    }
+
     // Custom Delimiter 검증 메서드
     public static int validateCustomDelimiter(String user_input) {
         int start_customDelimiterIndex = user_input.indexOf("//");
         int end_customDelimiterIndex = user_input.indexOf("\\n");
-
 
         if (end_customDelimiterIndex == -1) {
             throw new IllegalArgumentException(ErrorMessageTemplate.INVALID_CUSTOM_DELIMITER_FORMAT.getMessage());
